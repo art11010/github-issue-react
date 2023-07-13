9295aa5be672eacada75d6543d0a6ec1d48b26d8
YEZI <yezi.dev@gmail.com>
2023-07-13 20:57:26 +0900

    typescript cra 설치

---
+++ TypeScript-Ex/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => <>Init React TypeScript</>;
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <div>App</div>
+//   )
+// }
